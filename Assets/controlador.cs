6cabf2b9f1b67da89904f49c41344b2c081daf01
Sanca Venegas Martin Joel <splayer003@gmail.com>
2018-06-01 13:09:57 -0500

    controlador
    clase instanceada

---
+++ Assets/controlador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlador : MonoBehaviour {
+
+    public static controlador instance;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
